a9278b9ac8be20881199f97a6d1f79550942afb2
tauraamui <adampstringer@protonmail.com>
2023-09-20 17:11:04 +0100

    add better padding for top and sides

--- src/main.v
+++ src/main.v
@@ -13,11 +13,9 @@ struct App {
 fn main() {
 	mut app := &App{}
 	app.serve_static('/assets/black_wolf_face.png', 'src/assets/imgs/black_wolf_face.png')
-	app.serve_static('/assets/dark-grey.css', 'src/assets/css/dark-grey.css')
-	app.serve_static('/assets/dark.css', 'src/assets/css/dark.css')
-	app.serve_static('/assets/hack.css', 'src/assets/css/hack.css')
-	app.serve_static('/assets/solarized-dark.css', 'src/assets/css/solarized-dark.css')
-	app.serve_static('/assets/standard.css', 'src/assets/standard.css')
+	app.serve_static('/assets/css/dark-grey.css', 'src/assets/css/dark-grey.css')
+	app.serve_static('/assets/css/hack.css', 'src/assets/css/hack.css')
+	app.serve_static('/assets/css/site.css', 'src/assets/css/site.css')
 	// makes all static files available.
 
 	vweb.run(app, port)
