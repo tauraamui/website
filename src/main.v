656b05058c0a49904cd9a1efd6e620eeb9565bbe
tauraamui <adampstringer@protonmail.com>
2023-09-20 17:23:09 +0100

    embed css resources into binary and serve from custom handler

--- src/main.v
+++ src/main.v
@@ -3,6 +3,10 @@ module main
 import vweb
 
 const (
+	wolf_face_png = $embed_file('src/assets/imgs/black_wolf_face.png')
+	hack_css = $embed_file('src/assets/css/hack.css')
+	dark_grey_css = $embed_file('src/assets/css/dark-grey.css')
+	site_css = $embed_file('src/assets/css/site.css')
 	port = 8082
 )
 
@@ -13,12 +17,32 @@ struct App {
 fn main() {
 	mut app := &App{}
 	app.serve_static('/assets/black_wolf_face.png', 'src/assets/imgs/black_wolf_face.png')
-	app.serve_static('/assets/css/dark-grey.css', 'src/assets/css/dark-grey.css')
-	app.serve_static('/assets/css/hack.css', 'src/assets/css/hack.css')
-	app.serve_static('/assets/css/site.css', 'src/assets/css/site.css')
+	// app.serve_static('/assets/css/dark-grey.css', 'src/assets/css/dark-grey.css')
+	// app.serve_static('/assets/css/hack.css', 'src/assets/css/hack.css')
 	// makes all static files available.
 
 	vweb.run(app, port)
+}
+
+['/assets/css/:name']
+pub fn (mut app App) css(name string) vweb.Result {
+	match name {
+		"hack.css" {
+			app.set_content_type(app.req.header.get(.content_type) or { "" })
+			return app.ok(hack_css.to_string())
+		}
+		"dark-grey.css" {
+			app.set_content_type(app.req.header.get(.content_type) or { "" })
+			return app.ok(dark_grey_css.to_string())
+		}
+		"site.css" {
+			app.set_content_type(app.req.header.get(.content_type) or { "" })
+			return app.ok(site_css.to_string())
+		}
+		else {
+			return app.not_found()
+		}
+	}
 }
 
 ['/']
