ddc3cacacbd89ec81dcacd95e17b8b4e1337caea
tauraamui <adampstringer@protonmail.com>
2023-09-23 17:26:47 +0100

    feat: convert single empty lines into break tags

--- src/compile_blogs.v
+++ src/compile_blogs.v
@@ -24,10 +24,13 @@ fn compile_markdown_blogs_into_html_files() {
 		for !fd.eof() {
 			mut buffer := []u8{ len: 1024 }
 			read_bytes := fd.read_bytes_into_newline(mut &buffer) or { panic(err) }
+			// TODO:(tauraamui) -> handle stupidly long lines
 			if read_bytes < 1024 {
-				// TODO:(tauraamui) -> convert empty lines with just `\n` to `<br/>` and handle stupidly long lines
-				input_line := buffer[..read_bytes - if read_bytes > 1 { 1 } else { 0 }].bytestr()
-				output_line := markdown.to_html(input_line)
+				mut output_line := "<br/>"
+				if read_bytes > 1 {
+					input_line := buffer[..read_bytes - if read_bytes > 1 { 1 } else { 0 }].bytestr()
+					output_line = markdown.to_html(input_line)
+				}
 				wfd.writeln(output_line) or { println("unable to write to file: ${target}"); return }
 			}
 		}
