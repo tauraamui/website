9ffc2ae86fd05119fff9216d73b92ddb51551211
tauraamui <adampstringer@protonmail.com>
2023-09-26 17:05:11 +0100

    compile finished blog

--- src/resolve_blogs.v
+++ src/resolve_blogs.v
@@ -1,24 +1,24 @@
 module main
 
 const (
-	how_to_wash_up_correctly = $embed_file('./src/blog/how-to-wash-up-correctly.html', .zlib)
 	how_i_write_and_publish_blogs = $embed_file('./src/blog/how-i-write-and-publish-blogs.html', .zlib)
+	how_to_wash_up_correctly = $embed_file('./src/blog/how-to-wash-up-correctly.html', .zlib)
 )
 
 fn blogs_listing() []string {
 	return [
-		"how to wash up correctly"
 		"how i write and publish blogs"
+		"how to wash up correctly"
 	]
 }
 
 fn resolve_blog(name string) !string {
 	return match name {
-		"how-to-wash-up-correctly.html" {
-			how_to_wash_up_correctly.to_string()
-		}
 		"how-i-write-and-publish-blogs.html" {
 			how_i_write_and_publish_blogs.to_string()
+		}
+		"how-to-wash-up-correctly.html" {
+			how_to_wash_up_correctly.to_string()
 		}
 		else { error("unable to resolve blog") }
 	}
