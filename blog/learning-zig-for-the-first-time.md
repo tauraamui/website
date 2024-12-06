b2896afb0555dcd072d759d6906108189aa09eb0
tauraamui <adampstringer@protonmail.com>
2024-12-06 13:46:24 +0000

    start new zig blog article

---
+++ blog/learning-zig-for-the-first-time.md
@@ -0,0 +1,30 @@
+---
+date: 06/12/2024
+title: Learning Zig (for the first time basically)
+published: yes
+---
+
+
+# Learning Zig (for the first time really)
+#{read_time_seconds}
+### NOTE: This post is a WIP but I'm treating it as a public log of exploration
+
+So for various reasons I have not used Zig yet. The main reason being unclear documentation and (at the time) no official package management support. This has likely changed, and it felt like time to explore the language and the ecosystem once more.
+
+Today is 5th December 2024, and my first re-visiting of Zig and doing some searching around has revealed so far:
+
+- Zig zon is a thing
+- Its apparently the way you can pin/define concrete deps for your project!
+
+I cannot just use Zig for the sake of it, in isolation with no clear project or goal in mind, I _HAVE_ to learn by doing something "real". I can't help it, its like a disease.
+
+Therefore the first library I wanted to try out was this one:  https://github.com/ziglibs/ansi-term
+How can I get it? The options:
+
+- Git submodules (ew)
+- Directly cloning (feeling nauseous)
+- Using a build system to manage my deps (ahhhh)
+
+I figured out by frantically Google searching that Zig does technically have a native package manager these days, but it is completely undocumented from the official sources from what I could see. Some posts I can across implied this is deliberate due to it being in unsettled unstable stage of its creation.
+
+This post: https://ziggit.dev/t/how-to-add-a-dependency-to-build-zig-zon/6769 helped me learn that it is indeed possible (thank god) to install deps even if the repo that contains the desired code has no associated releases.
--- src/resolve_blogs.v
+++ src/resolve_blogs.v
@@ -1,9 +1,10 @@
 module main
 
-const how_i_write_and_publish_blogs = $embed_file('./src/blog/how-i-write-and-publish-blogs.html', .zlib)
-const how_to_wash_up_correctly = $embed_file('./src/blog/how-to-wash-up-correctly.html', .zlib)
-const where_did_you_buy_your_license = $embed_file('./src/blog/where-did-you-buy-your-license.html', .zlib)
+const learning_zig_for_the_first_time = $embed_file('./src/blog/learning-zig-for-the-first-time.html', .zlib)
 const lilly_devlog_1_qsort_macos_bug = $embed_file('./src/blog/lilly-devlog-1-qsort-macos-bug.html', .zlib)
+const where_did_you_buy_your_license = $embed_file('./src/blog/where-did-you-buy-your-license.html', .zlib)
+const how_to_wash_up_correctly = $embed_file('./src/blog/how-to-wash-up-correctly.html', .zlib)
+const how_i_write_and_publish_blogs = $embed_file('./src/blog/how-i-write-and-publish-blogs.html', .zlib)
 
 struct Listing {
 	date string
@@ -12,9 +13,10 @@ struct Listing {
 }
 fn blogs_listing() []Listing {
 	return [
-		Listing { date: "26/09/2023" title: "How I write and publish blogs", file_name: "how-i-write-and-publish-blogs" }
-		Listing { date: "24/09/2023" title: "How to correctly wash up", file_name: "how-to-wash-up-correctly" }
+		Listing { date: "06/12/2024" title: "Learning Zig (for the first time basically)", file_name: "learning-zig-for-the-first-time" }
 		Listing { date: "05/10/2023" title: "You can't drive (yes you)", file_name: "where-did-you-buy-your-license" }
+		Listing { date: "24/09/2023" title: "How to correctly wash up", file_name: "how-to-wash-up-correctly" }
+		Listing { date: "26/09/2023" title: "How I write and publish blogs", file_name: "how-i-write-and-publish-blogs" }
 	]
 }
 
@@ -25,17 +27,20 @@ struct Post {
 
 fn resolve_blog(name string) !Post {
 	return match name {
-		"how-i-write-and-publish-blogs" {
-			Post { title: "How I write and publish blogs", content: how_i_write_and_publish_blogs.to_string() }
+		"learning-zig-for-the-first-time" {
+			Post { title: "Learning Zig (for the first time basically)", content: learning_zig_for_the_first_time.to_string() }
 		}
-		"how-to-wash-up-correctly" {
-			Post { title: "How to correctly wash up", content: how_to_wash_up_correctly.to_string() }
+		"lilly-devlog-1-qsort-macos-bug" {
+			Post { title: "Lilly Devlog 1 - QSort macOS bug", content: lilly_devlog_1_qsort_macos_bug.to_string() }
 		}
 		"where-did-you-buy-your-license" {
 			Post { title: "You can't drive (yes you)", content: where_did_you_buy_your_license.to_string() }
 		}
-		"lilly-devlog-1-qsort-macos-bug" {
-			Post { title: "Lilly Devlog 1 - QSort macOS bug", content: lilly_devlog_1_qsort_macos_bug.to_string() }
+		"how-to-wash-up-correctly" {
+			Post { title: "How to correctly wash up", content: how_to_wash_up_correctly.to_string() }
+		}
+		"how-i-write-and-publish-blogs" {
+			Post { title: "How I write and publish blogs", content: how_i_write_and_publish_blogs.to_string() }
 		}
 		else { error("unable to resolve blog") }
 	}
